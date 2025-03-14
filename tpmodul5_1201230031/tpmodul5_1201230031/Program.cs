bd7169aac1336ae0ea4e2f57a4e126173b2be241
Ahab131 <hakimalbaihaqy100@gmail.com>
2025-03-15 06:39:41 +0700

    Menambahkan class DataGeneric dengan method PrintData menggunakan generic

---
+++ tpmodul5_1201230031/tpmodul5_1201230031/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace tpmodul5_1201230031
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Membuat instance dari class DataGeneric dengan tipe string
+            DataGeneric<string> data = new DataGeneric<string>("1201230031"); // Ganti dengan NIM Anda
+
+            // Memanggil method PrintData
+            data.PrintData();
+        }
+    }
+}
