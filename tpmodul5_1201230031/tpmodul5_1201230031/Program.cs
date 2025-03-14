5b5c28a8c5cc2fdec1520582398e2632660e3f05
Ahab131 <hakimalbaihaqy100@gmail.com>
2025-03-15 06:20:33 +0700

    menambahkan class HaloGeneric dengan method SapaUser

---
+++ tpmodul5_1201230031/tpmodul5_1201230031/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace tpmodul5_1201230031
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HaloGeneric<string> halo = new HaloGeneric<string>();
+
+            halo.SapaUser("John"); 
+        }
+    }
+}
