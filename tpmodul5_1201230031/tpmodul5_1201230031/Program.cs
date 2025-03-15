5eed065a1da919d002f4b5d223620479512fd99a
Ahab131 <hakimalbaihaqy100@gmail.com>
2025-03-15 07:17:11 +0700

    Add files via upload

---
+++ tpmodul5_1201230031/tpmodul5_1201230031/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace tpmodul5_1201230031
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+<<<<<<< HEAD
+            HaloGeneric<string> halo = new HaloGeneric<string>();
+
+            halo.SapaUser("John"); 
+=======
+            // Membuat instance dari class DataGeneric dengan tipe string
+            DataGeneric<string> data = new DataGeneric<string>("1201230031"); // Ganti dengan NIM Anda
+
+            // Memanggil method PrintData
+            data.PrintData();
+>>>>>>> generic-class
+        }
+    }
+}
